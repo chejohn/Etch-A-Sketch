f00bd1210a66963f823b7cf6ac4864d41ef9b0c2
Ché John <chejohn16@gmail.com>
2021-05-12 02:24:23 -0400

    fully implemented etch-a-sketch

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,26 +1,61 @@
 
-
-function getRandomRGB() {
-    const randRed = Math.floor(Math.random() * 256);
-    const randGreen = Math.floor(Math.random() * 256);
-    const randBlue = Math.floor(Math.random() * 256);
-    return `rgb(${randRed},${randGreen}, ${randBlue})`
+/*
+params: current hover count
+return: random rgb value that reduces in tone by 10% 
+        for each call after first until black.
+*/
+function getRandomRGB(currentHoverCount) {
+    const maxValue = Math.floor(256 * (1 - currentHoverCount/10));
+    const randRed = Math.floor(Math.random() * maxValue);
+    const randGreen = Math.floor(Math.random() * maxValue);
+    const randBlue = Math.floor(Math.random() * maxValue);
+    return `rgb(${randRed},${randGreen}, ${randBlue})`;
 }
 
-/*
-params: row and column values
-returns: a drawn grid
-*/
+function alterCellColor() {
+    this.style.setProperty('background-color', getRandomRGB(this.getHoverCount()));
+}
+
+function setHoverCount() {
+    this.hoverCount++;
+}
+
 function drawGrid(rows, cols) {
     gridContainer.style.setProperty('--grid-rows', rows);
     gridContainer.style.setProperty('--grid-cols', cols);
 
     for (let i = 0; i < (rows * cols); i++) {
-        let cell = document.createElement('div');
+        const cell = document.createElement('div');
         cell.style.border = 'solid black 1px';
         gridContainer.appendChild(cell).className = 'grid-item';
     }
 }
 
+function refreshPage() {
+    window.location.reload();
+}
+
+// returns: user number if input <= 100
+function validateGridSize() {
+    const userNum = Number(prompt('Enter a single value n for grid size n x n (n must be <= 100): '));
+    while (userNum > 100) {
+        userNum = Number(prompt('Enter a single value n for grid size n x n (n must be <= 100): '));
+    }
+    return userNum;
+}
+
+const userNum = validateGridSize();
+
 const gridContainer = document.getElementById('grid-container');
-drawGrid(5,5);
+drawGrid(userNum,userNum);
+
+const allCells = document.querySelectorAll('.grid-item');
+allCells.forEach((cell) => {
+    cell.hoverCount = 0;
+    cell.addEventListener('mouseover', alterCellColor);
+    cell.addEventListener('mouseover', setHoverCount);
+    cell.getHoverCount = () => cell.hoverCount;
+})
+
+const button = document.getElementById('clear-button');
+button.addEventListener('click', refreshPage);
